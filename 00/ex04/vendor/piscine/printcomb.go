df10586f4a4fa52643cf7785df542c273b631705
yuki-katayama <katayu810@gmail.com>
2022-04-26 02:17:02 +0900

    fix: 改行抜けてtあ

--- 00/ex04/vendor/piscine/printcomb.go
+++ 00/ex04/vendor/piscine/printcomb.go
@@ -4,19 +4,28 @@ import (
 	"ft"
 )
 
+func PrintThreeDigits(first rune, second rune, third rune) {
+	ft.PrintRune(first);
+	ft.PrintRune(second);
+	ft.PrintRune(third);
+}
+
+func PrintSeparate() {
+	ft.PrintRune(',');
+	ft.PrintRune(' ');
+}
+
 func PrintComb() {
 	for x := '0'; x <= '7'; x++ {
 		for y := x + 1; y <= '8'; y++ {
 			for z := y + 1; z <= '9'; z++ {
-				ft.PrintRune(x);
-				ft.PrintRune(y);
-				ft.PrintRune(z);
+				PrintThreeDigits(x, y, z);
 				if x == '7' {
 					break;
 				}
-				ft.PrintRune(',');
-				ft.PrintRune(' ');
+				PrintSeparate();
 			}
 		}
 	}
+	ft.PrintRune('\n');
 }
