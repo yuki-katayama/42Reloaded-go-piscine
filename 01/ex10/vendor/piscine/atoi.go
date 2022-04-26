194320a1a26fec5aa981a82a9c360d0581b3d6f7
yuki-katayama <katayu810@gmail.com>
2022-04-27 01:57:16 +0900

    add: 01_ex10

---
+++ 01/ex10/vendor/piscine/atoi.go
@@ -0,0 +1,30 @@
+package piscine
+
+func Atoi(s string) int {
+	if s == "" {
+		return 0
+	}
+	runes := []rune(s)
+	var res int = 0
+	var neg bool = false
+	if (runes[0] == '-' || runes[0] == '+') {
+		if (runes[0] == '-') {
+			neg = true;
+		}
+		runes[0] = '0';
+	}
+	for i, r := range runes {
+		if (!(r >= '0' && r <= '9')) {
+			return (0);
+		}
+		res += int(r - '0');
+		if (i == len(runes) - 1) {
+			break ;
+		}
+		res *= 10;
+	}
+	if (neg == true) {
+		res *= -1;
+	}
+	return (res);
+}
