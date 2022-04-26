4dd215ba8a17e92d1d53ce0320615ff2ff430861
yuki-katayama <katayu810@gmail.com>
2022-04-27 01:34:38 +0900

    add: 01_ex08

---
+++ 01/ex08/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	fmt.Println(piscine.BasicAtoi("12a45"))
+	fmt.Println(piscine.BasicAtoi("12345"))
+	fmt.Println(piscine.BasicAtoi("0000000012345"))
+	fmt.Println(piscine.BasicAtoi("000000"))
+}
