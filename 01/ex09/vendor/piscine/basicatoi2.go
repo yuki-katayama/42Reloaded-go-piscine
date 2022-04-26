f4068c3e7554109db317d7a5685a0fd5c5b08c3d
yuki-katayama <katayu810@gmail.com>
2022-04-27 01:57:38 +0900

    fix: atoiの機能を少なくしました

--- 01/ex09/vendor/piscine/basicatoi2.go
+++ 01/ex09/vendor/piscine/basicatoi2.go
@@ -1,24 +1,20 @@
 package piscine
 
-func BasicAtoi2(s string) int {
+func Atoi(s string) int {
 	if s == "" {
 		return 0
 	}
 	runes := []rune(s)
 	var res int = 0
-	var neg bool = false
 	for i, r := range runes {
 		if (!(r >= '0' && r <= '9')) {
 			return (0);
 		}
-		res -= int(r - '0');
+		res += int(r - '0');
 		if (i == len(runes) - 1) {
 			break ;
 		}
 		res *= 10;
-	}
-	if (neg == false) {
-		res *= -1;
 	}
 	return (res);
 }
